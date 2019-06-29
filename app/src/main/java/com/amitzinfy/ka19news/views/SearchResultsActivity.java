934ba38931d2cd323da4e2a5cc0ae8b0ebf7f841
Jassem Abbas <jassi050@gmail.com>
2019-06-29 15:22:39 +0530

    Create SearchResults activity
    Signed-off-by: Jassem Abbas <jassi050@gmail.com>

---
+++ app/src/main/java/com/amitzinfy/ka19news/views/SearchResultsActivity.java
@@ -0,0 +1,16 @@
+package com.amitzinfy.ka19news.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amitzinfy.ka19news.R;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
