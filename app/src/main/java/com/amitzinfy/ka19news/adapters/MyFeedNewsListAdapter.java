114655f2ff0c80a3286aedbb7def9f87e40a733e
Jassem Abbas <jassi050@gmail.com>
2019-07-16 13:20:37 +0530

    Create News details view, viewModel, repository
    Signed-off-by: Jassem Abbas <jassi050@gmail.com>

--- app/src/main/java/com/amitzinfy/ka19news/adapters/MyFeedNewsListAdapter.java
+++ app/src/main/java/com/amitzinfy/ka19news/adapters/MyFeedNewsListAdapter.java
@@ -143,4 +143,5 @@ public class MyFeedNewsListAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         void setItemToggleButton(ToggleButton toggleButton, int position);
     }
 
+
 }
---
+++ app/src/main/java/com/amitzinfy/ka19news/views/NewsDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.amitzinfy.ka19news.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amitzinfy.ka19news.R;
+
+public class NewsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+    }
+}
