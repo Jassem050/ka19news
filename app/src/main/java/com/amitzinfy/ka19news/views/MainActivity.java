0ce3b408eae199cef3bf28646a4437bfa3f3b86f
Jassem Abbas <jassi050@gmail.com>
2019-07-31 15:26:50 +0530

    Add navigation drawer and nav items
    Signed-off-by: Jassem Abbas <jassi050@gmail.com>

--- app/src/main/java/com/amitzinfy/ka19news/views/MainActivity.java
+++ app/src/main/java/com/amitzinfy/ka19news/views/MainActivity.java
@@ -3,20 +3,25 @@ package com.amitzinfy.ka19news.views;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.amitzinfy.ka19news.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener,
         HeadLineFragment.OnFragmentInteractionListener, FavouriteFragment.OnFragmentInteractionListener,
         DynamicTabFragment.OnFragmentInteractionListener{
 
     private BottomNavigationView bottomNavigationView;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,33 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
         setContentView(R.layout.activity_main);
         init();
         setUpBottomNavView();
+        setUpNavigationView();
         loadFragment(HomeFragment.newInstance("home","home"));
     }
 
     private void init(){
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation);
+    }
+
+    private void setUpNavigationView(){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                item.setChecked(true);
+                switch (item.getItemId()){
+                    case R.id.english:
+                        Toast.makeText(MainActivity.this, "english", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.kannada:
+                        Toast.makeText(MainActivity.this, "Kannada", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     private void setUpBottomNavView(){
